e5418e5bb46cd708cee5d9c3d1a1bb8c5b96682d
Tomaz Muraus <tomaz@tomaz.me>
2020-10-11 22:27:33 +0200

    Update default value handling in wx server configuration module.

--- wx_server/wx_server/configuration.py
+++ wx_server/wx_server/configuration.py
@@ -7,9 +7,19 @@ import structlog
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = os.path.abspath(os.path.join(BASE_DIR, "../"))
 
-LOG = structlog.get_logger()
+DEFAULT_CONFIG_PATH = os.path.abspath(os.path.join(BASE_DIR, '../conf/wx_server.conf'))
+CONFIG_PATH = os.environ.get("WX_SERVER_CONFIG_PATH", DEFAULT_CONFIG_PATH)
+
+DEFAULT_VALUES = {
+    "main": {
+        "data_dir": "{rootdir}/data/",
+        "logging_config": "{rootdir}/conf/logging.conf",
+    }
+}
 
 CONFIG = None
+
+LOG = structlog.get_logger()
 
 
 def load_and_parse_config(config_path: str, validate=True):
@@ -23,6 +33,7 @@ def load_and_parse_config(config_path: str, validate=True):
         raise ValueError("Config file %s doesn't exist" % (config_path))
 
     config = configparser.ConfigParser()
+    config.read_dict(DEFAULT_VALUES)
     config.read(config_path)
 
     if validate:
@@ -34,36 +45,33 @@ def load_and_parse_config(config_path: str, validate=True):
 
 
 def validate_config(config):
-    data_dir = config["main"].get("data_dir", None)
-
-    if not data_dir:
-        config["main"]["data_dir"] = "{rootdir}/data/"
-
+    data_dir = config["main"]["data_dir"]
     config["main"]["data_dir"] = config["main"]["data_dir"].replace("{rootdir}", ROOT_DIR)
 
     if not os.path.isdir(config["main"]["data_dir"]):
-        raise ValueError("Data directory %s doesn't exist or it it's not a directory" %
+        raise ValueError("Data dir %s doesn't exist or it's not a directory" %
                          (config["main"]["data_dir"]))
 
-    logging_config = config["main"].get("logging_config", None)
 
-    if not data_dir:
-        config["main"]["logging_config"] = "{rootdir}/conf/logging.conf"
-
+    logging_config = config["main"]["logging_config"]
     config["main"]["logging_config"] = config["main"]["logging_config"].replace("{rootdir}", ROOT_DIR)
 
     if not os.path.isfile(config["main"]["logging_config"]):
-        raise ValueError("Logging config %s doesn't exist or it it's not a file" %
+        raise ValueError("Logging config %s doesn't exist or it's not a file" %
                          (config["main"]["logging_config"]))
-
 
     return config
 
 
 def get_config():
+    """
+    Retrieved loaded and parsed config instance.
+
+    If config hasn't be loaded yet, it will be loaded and parsed by this method.
+    """
     global CONFIG
 
     if not CONFIG:
-        load_and_parse_config()
+        load_and_parse_config(CONFIG_PATH)
 
     return CONFIG

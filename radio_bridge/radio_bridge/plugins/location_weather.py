09756cac2aabc0cf5f9faecc15ff5f507434a628
Tomaz Muraus <tomaz@tomaz.me>
2020-10-20 20:52:13 +0200

    Fix typo.

--- radio_bridge/radio_bridge/plugins/location_weather.py
+++ radio_bridge/radio_bridge/plugins/location_weather.py
@@ -59,7 +59,7 @@ class LocationWeatherPlugin(BaseDTMFWithDataPlugin):
     DESCRIPTION = "Current weather for location"
     REQUIRES_INTERNET_CONNECTION = True
     # Second two characters and city code - e.g. 01 - Ljubljana, 02 - Maribor, etc.
-    DTMF_SEQUENCE = get_config().get("plugin:local_weather", "dtmf_sequence", fallback="35??")
+    DTMF_SEQUENCE = get_config().get("plugin:location_weather", "dtmf_sequence", fallback="35??")
 
     def run(self, sequence: str):
         if sequence not in LOCATION_CODE_TO_CITY_MAP:
